7335778931b5a522f890450e6353c2812100f90a
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-06-24 19:23:13 -0300

    Updating and organizing files, references and path resolution.

--- src/components/root/Root.tsx
+++ src/components/root/Root.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './Root.css'
+import App from 'components/app/App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
