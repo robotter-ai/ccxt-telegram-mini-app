922eb422e41f9868d81c3c4d12172da8f4c54063
Hudson Oliveira <hudson.oliveira@funttastic.com>
2024-06-27 18:32:58 -0300

    Adding exchange proxy

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,13 +3,13 @@ import './App.css';
 // import React, { useRef, useState } from 'react';
 // import logo from 'assets/images/logo/exchange.svg';
 // import ccxt from 'assets/scripts/ccxt/ccxt.browser'
-import Table from "./Table"
+import Table from "../views/Table"
 
 
 function App() {
 	const hasRun = useRef(false);
 
-  // const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
 
 	React.useEffect(() => {
 		if (hasRun.current) return;
@@ -18,7 +18,10 @@ function App() {
 
 		const initialize = async () => {
 			try {
-				console.log(ccxt.exchanges);
+				// console.log(ccxt.exchanges);
+
+				// console.log(import.meta.env.VITE_EXCHANGE_API_KEY);
+				// console.log(import.meta.env.VITE_EXCHANGE_API_SECRET);
 
 				const exchangeId = import.meta.env.VITE_EXCHANGE_ID;
 				const exchangeClass = ccxt[exchangeId];
@@ -26,14 +29,17 @@ function App() {
 					'apiKey': import.meta.env.VITE_EXCHANGE_API_KEY,
 					'secret': import.meta.env.VITE_EXCHANGE_API_SECRET,
 				});
+				exchange.proxyUrl = 'http://localhost:8080/';
+
+				exchange.setSandboxMode(true);
 
 				// console.log(exchange.describe());
-				console.log(await exchange.fetchCurrencies());
-				console.log(await exchange.fetchMarkets());
+				// console.log(await exchange.fetchCurrencies());
+				// console.log(await exchange.fetchMarkets());
 				console.log(await exchange.fetchTickers());
-				console.log(await exchange.fetchOrderBook('BTC/USDC'));
-				console.log(await exchange.fetchBalance());
-				console.log(await exchange.fetchOpenOrders('BTC/USDC'));
+				// console.log(await exchange.fetchOrderBook('BTC/USDC'));
+				// console.log(await exchange.fetchBalance());
+				// console.log(await exchange.fetchOpenOrders('BTC/USDC'));
 			} catch (exception) {
 				console.error(exception);
 			}
@@ -42,11 +48,11 @@ function App() {
 		initialize();
 	}, []);
 
-	const getOpenOrders = () => {
-		return {
-
-		}
-	}
+	// const getOpenOrders = () => {
+	// 	return {
+	//
+	// 	}
+	// }
 
   return (
     <>
