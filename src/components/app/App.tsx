f156d8309a7159ed549c5bffa5fbd08572374447
Hudson Oliveira <hudson.oliveira@funttastic.com>
2024-07-10 09:52:16 -0300

    Adding a new table component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,17 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-// import React, { useRef, useState } from 'react';
 // import logo from 'assets/images/logo/exchange.svg';
 // import ccxt from 'assets/scripts/ccxt/ccxt.browser'
-import Table from "../views/Table"
+// import Table from "../views/Table"
+import EnhancedTable from 'components/views/EnhancedTable';
+
+// const OrderInfo = ({ order }) => {
+// 	return (
+// 	<div style={{ marginBottom: '20px' }}>
+// 		<pre>{JSON.stringify(order, null, 2)}</pre>
+// 	</div>
+// 	);
+//   };
 
 
 function App() {
 	const hasRun = useRef(false);
+	const [orders, setOrders] = useState(null);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-    // const [count, setCount] = useState(0);
-
-	React.useEffect(() => {
+	useEffect(() => {
 		if (hasRun.current) return;
 
 		hasRun.current = true;
@@ -36,28 +45,57 @@ function App() {
 				// console.log(exchange.describe());
 				// console.log(await exchange.fetchCurrencies());
 				// console.log(await exchange.fetchMarkets());
-				console.log(await exchange.fetchTickers());
+				// console.log(await exchange.fetchTickers());
 				// console.log(await exchange.fetchOrderBook('BTC/USDC'));
 				// console.log(await exchange.fetchBalance());
-				// console.log(await exchange.fetchOpenOrders('BTC/USDC'));
-			} catch (exception) {
-				console.error(exception);
-			}
+				// console.log(await exchange.createOrder('BTC/USDT','market', 'sell', 0.01, '51000'))
+				// console.log(JSON.stringify((await exchange.fetchOpenOrders('BTC/USDT'))));
+
+				const fetchedOrders = await exchange.fetchOpenOrders('BTC/USDT');
+        console.log('Fetched Orders:', fetchedOrders);
+
+        const formattedOrders = fetchedOrders.map((order, index) => ({
+          id: index,
+          market: order.symbol,
+          status: order.status,
+          side: order.side,
+          amount: order.amount,
+          price: order.price,
+          datetime: new Date(order.timestamp).toLocaleString(),
+        }));
+        console.log('Formatted Orders:', formattedOrders);
+
+        setOrders(formattedOrders);
+      } catch (exception) {
+        setError(exception.message);
+      } finally {
+        setLoading(false);
+    }
 		};
 
-		initialize();
-	}, []);
-
-	// const getOpenOrders = () => {
-	// 	return {
-	//
-	// 	}
-	// }
+    initialize();
+  }, []);
 
   return (
-    <>
-		<Table/>
-    </>
+    // <>
+	// 	<Table/>
+	// 	<div>
+    //     <h1 className='text-lg font-bold'>Order Information</h1>
+    //     {loading && <div>Loading...</div>}
+    //     {error && <div>Error: {error}</div>}
+    //     {orders && orders.length > 0 ? (
+    //       orders.map((order, index) => <OrderInfo key={index} order={order} />)
+    //     ) : (
+    //       !loading && <div>No open orders</div>
+    //     )}
+    //   </div>
+    // </>
+	<div>
+      <h1>Order Information</h1>
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error}</div>}
+      {!loading && !error && <EnhancedTable rows={orders} />}
+    </div>
   )
 }
 
--- src/components/root/Root.tsx
+++ src/components/root/Root.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './Root.css'
 import App from 'components/app/App.tsx'
+// import { Provider } from 'react-redux'
+// import { Router } from 'react-router'
+// import { reduxStore } from 'model/state/redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')!)
 	.render(
 		<React.StrictMode>
-			<Provider store={reduxStore}>
+			{/* <Provider store={reduxStore}>
 				<Router/>
-			</Provider>
+			</Provider> */}
+			<App/>
 		</React.StrictMode>,
 	);
