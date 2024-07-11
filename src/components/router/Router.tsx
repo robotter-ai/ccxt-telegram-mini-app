4fdcdb015aa38e40a09b8f7a2db13aa2286bde92
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-07-11 16:57:05 -0300

    Updating view components.

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -4,40 +4,67 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Orders from 'components/views/orders/Orders.tsx';
 import DevelopmentHome from 'components/views/development/Development';
 import SignIn from 'components/views/sign_in/SignIn';
+import { connect } from 'react-redux';
 
-/**
- *
- */
-export class Router extends React.Component {
+const mapStateToProps = (state: any) => ({
+	isSignedIn: state.api.isSignedIn,
+});
 
-	/**
-	 *
-	 */
+interface RouterProps {
+	isSignedIn: boolean;
+}
+
+class RouterStructure extends React.Component<RouterProps> {
 	render() {
+		const { isSignedIn } = this.props;
+
+		const SignInRedirect = () => <Navigate to="/signIn" />;
+		const RootRedirect = () => <Navigate to="/" />;
+
+		// @ts-ignore
+		const normalizeRoute = (route: string) => {
+			return route
+				.toLowerCase()
+				.replace(/[-_](.)/g, (_, char) => char.toUpperCase());
+		};
+
 		return (
 			<BrowserRouter>
 				<div>
 					<Routes>
 						{/*
-								Public
+							Public
 						*/}
-						<Route path="/signIn" element={<SignIn/>} />
-						<Route path="/sigin" element={<Navigate to="/signIn" />} />
-						<Route path="/login" element={<Navigate to="/signIn" />} />
+						<Route path="/signIn" element={<SignIn />} />
 
 						{/*
-								User
+							User
 						*/}
-						<Route path="/" element={<Orders/>} />
+						<Route path="/" element={<Navigate to="/orders" />} />
 						<Route path="/home" element={<Navigate to="/" />} />
+						<Route path="/orders" element={isSignedIn ? <Orders /> : <SignInRedirect />} />
 
 						{/*
-								Development
+							Development
 						*/}
-						<Route path="/development" element={<DevelopmentHome/>} />
+						<Route path="/development" element={isSignedIn ? <DevelopmentHome /> : <SignInRedirect />} />
+
+						{/*
+							Redirect unknown routes
+						*/}
+						<Route
+							path="*"
+							element={
+								isSignedIn ? <RootRedirect /> : <SignInRedirect />
+							}
+						/>
 					</Routes>
 				</div>
 			</BrowserRouter>
 		);
 	}
 }
+
+const Router = connect(mapStateToProps)(RouterStructure);
+
+export default Router;
