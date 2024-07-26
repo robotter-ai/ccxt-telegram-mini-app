0bb64ddba2d0ac27a6a7a80f69b5adc899b701f7
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-07-26 10:58:50 -0300

    Fixing initialization and avoiding store getIn error.

--- src/components/root/Root.tsx
+++ src/components/root/Root.tsx
@@ -7,15 +7,19 @@ import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// noinspection UnnecessaryLocalVariableJS
-const reduxStore = (await import('model/state/redux/store')).reduxStore;
+const initialize = async () => {
+	// noinspection UnnecessaryLocalVariableJS
+	const reduxStore = (await import('model/state/redux/store')).reduxStore;
 
-ReactDOM.createRoot(document.getElementById('root')!)
-	.render(
-		// <React.StrictMode>
+	ReactDOM.createRoot(document.getElementById('root')!)
+		.render(
+			// <React.StrictMode>
 			<Provider store={reduxStore}>
 				<ToastContainer/>
 				<Router/>
 			</Provider>
-		// </React.StrictMode>
-	);
+			// </React.StrictMode>
+		);
+}
+
+initialize()
