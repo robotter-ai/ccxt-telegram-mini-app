23d2c87d86917daef7d126b750ae049d2e873f96
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-07-30 19:10:23 -0300

    Reverting change temporarily.

--- src/components/views/orders/Orders.tsx
+++ src/components/views/orders/Orders.tsx
@@ -70,6 +70,7 @@ class OrdersStructure extends Base<OrdersProps, OrdersState, OrdersSnapshot> {
 					environment: `${import.meta.env.VITE_EXCHANGE_ENVIRONMENT}`,
 					method: 'fetch_open_orders',
 					parameters: {
+						symbol: 'tSOLtUSDC',
 					},
 				},
 				this.props.handleUnAuthorized
