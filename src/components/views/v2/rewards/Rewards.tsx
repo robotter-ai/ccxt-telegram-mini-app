7e421becd1bae0aad79092740df49162a8679273
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-08-28 15:11:20 -0300

    readjusting api functions in components.

--- src/components/views/v2/rewards/Rewards.tsx
+++ src/components/views/v2/rewards/Rewards.tsx
@@ -7,10 +7,12 @@ import {apiGetFetchTickers} from 'model/service/api';
 import { Spinner } from 'components/views/v2/layout/spinner/Spinner';
 import {Style} from "@mui/icons-material";
 import axios from "axios";
-import React from "react";
 
 
-interface RewardsProps extends BaseProps {}
+
+interface RewardsProps extends BaseProps {
+	data: any;
+}
 
 interface RewardsState extends BaseState {
 	isLoading: boolean;
@@ -18,11 +20,13 @@ interface RewardsState extends BaseState {
 	claimStatus: boolean;
 }
 
-const mapStateToProps = (state: any) => ({});
+const mapStateToProps = () => ({});
+
 
 class RewardsStructure extends Base<RewardsProps, RewardsState> {
 	static defaultProps: Partial<BaseProps> = {};
-	private handleClaimReward: React.MouseEventHandler<HTMLButtonElement> | undefined;
+	handleClaimReward: React.MouseEventHandler<HTMLButtonElement> | undefined;
+	properties: any;
 
 	constructor(props: RewardsProps) {
 		super(props);
@@ -30,7 +34,8 @@ class RewardsStructure extends Base<RewardsProps, RewardsState> {
 		this.state = {
 			isLoading: false,
 			error: undefined,
-		} as Readonly<State>;
+			claimStatus: false,
+		} as Readonly<RewardsState>;
 
 		this.properties.setIn('recurrent.5s.intervalId', undefined);
 		this.properties.setIn('recurrent.5s.delay', 5 * 1000);
@@ -42,8 +47,9 @@ class RewardsStructure extends Base<RewardsProps, RewardsState> {
 	}
 
 	async componentWillUnmount() {
-		if (this.properties.getIn<number>('recurrent.5s.intervalId')) {
-			clearInterval(this.properties.getIn<number>('recurrent.5s.intervalId'));
+		const intervalId = this.properties.getIn('recurrent.5s.intervalId') as number;
+		if (intervalId) {
+			clearInterval(intervalId);
 		}
 	}
 
@@ -106,6 +112,7 @@ class RewardsStructure extends Base<RewardsProps, RewardsState> {
 
 	async doRecurrently() {
 		const recurrentFunction = async (symbol: string) => {
+			// @ts-ignore
 			try {
 				const response = await apiGetFetchTickers(
 					{
@@ -118,7 +125,7 @@ class RewardsStructure extends Base<RewardsProps, RewardsState> {
 					if (response.data?.title) {
 						const message = response.data.title;
 
-						this.setState({ error: message });
+						this.setState({error: message});
 						toast.error(message);
 
 						return;
@@ -139,147 +146,152 @@ class RewardsStructure extends Base<RewardsProps, RewardsState> {
 						return;
 					}
 				}
+			}
 
-	render() {
-		const { isLoading, error, claimStatus } = this.state;
+			render()
+			{
+				const {isLoading, error, claimStatus} = this.state;
 
-		if (isLoading) {
-			return <div>Loading...</div>;
+				if (isLoading) {
+					return <div>Loading...</div>;
+				}
+
+				if (error) {
+					return <div className="text-red-500">{error}</div>;
+				}
+
+				const totalCubePoints = 1160;
+
+				return (
+					<div className="flex flex-col h-full">
+						<div className="flex-grow overflow-hidden">
+							<div className="mb-4 text-center">
+								<div className="mt-10 text-sm text-gray-400">Total Cube Points</div>
+								<div className="mb-10 text-2xl font-bold text-white">{totalCubePoints.toLocaleString()}</div>
+							</div>
+							<div className="mb-8">
+								<table className="w-full bg-[#393939] text-white">
+									<thead className="sticky top-0 bg-[#393939]">
+									<tr>
+										<th className="px-4 py-2 text-left text-[#FE8A00]">Rewards</th>
+										<th className="px-4 py-2 text-right text-[#FE8A00] w-4/12">Points</th>
+									</tr>
+									</thead>
+									<tbody>
+									<tr className="border-b border-gray-600">
+										<td className="px-4 py-2">
+											<div className="flex flex-col">
+												<span className="text-lg leading-none">Invite Friends</span>
+												<button className="mt-2 px-2 py-1 text-sm bg-[#FE8A00] text-black rounded hover:bg-orange-600">
+													Send Referral
+												</button>
+											</div>
+										</td>
+										<td className="px-4 py-2 text-right">
+											<div className="text-lg leading-none">+80</div>
+										</td>
+									</tr>
+									<tr className="border-b border-gray-600">
+										<td className="px-4 py-2">
+											<div className="flex flex-col">
+												<span className="text-lg leading-none">Daily Check-in</span>
+												<span className="text-xs text-gray-400 mt-1">Streak: 5 Days</span>
+												<button
+													className={`mt-2 px-2 py-1 text-sm rounded ${claimStatus ? 'bg-white text-[#FE8A00] border border-[#FE8A00]' : 'bg-[#FE8A00] text-black hover:bg-orange-600'}`}
+													onClick={this.handleClaimReward}
+													disabled={claimStatus}
+												>
+													{claimStatus ? 'Claimed' : 'Claim Reward'}
+												</button>
+											</div>
+										</td>
+										<td className="px-4 py-2 text-right">
+											<div className="text-lg leading-none">+80</div>
+										</td>
+									</tr>
+									</tbody>
+								</table>
+							</div>
+							<div>
+								<table className="w-full bg-[#393939] text-white">
+									<thead className="sticky top-0 bg-[#393939]">
+									<tr>
+										<th className="px-4 py-2 text-left text-[#FE8A00]">Tasks</th>
+										<th className="px-4 py-2 text-right text-[#FE8A00] w-4/12">Points</th>
+									</tr>
+									</thead>
+									<tbody>
+									<tr className="border-b border-gray-600">
+										<td className="px-4 py-2">
+											<div className="flex flex-col">
+												<span className="text-lg leading-none">Completed Tasks</span>
+											</div>
+										</td>
+										<td className="px-4 py-2 text-right">
+											<div className="text-lg leading-none">+1000</div>
+										</td>
+									</tr>
+									<tr className="border-b border-gray-600">
+										<td className="px-4 py-2">
+											<div className="flex flex-col">
+												<span className="text-lg leading-none">Complete a trade &gt; $250</span>
+												<button
+													className="mt-2 px-2 py-1 text-sm bg-[#393939] text-[#FE8A00] rounded border border-[#FE8A00] hover:bg-gray-600">
+													Trade
+												</button>
+											</div>
+										</td>
+										<td className="px-4 py-2 text-right">
+											<div className="text-lg leading-none">
+												<span className="text-sm">Reward: </span>500
+											</div>
+										</td>
+									</tr>
+									<tr className="border-b border-gray-600">
+										<td className="px-4 py-2">
+											<div className="flex flex-col">
+												<span className="text-lg leading-none">Trade &gt; $2500 ZEX</span>
+												<button
+													className="mt-2 px-2 py-1 text-sm bg-[#393939] text-[#FE8A00] rounded border border-[#FE8A00] hover:bg-gray-600">
+													Trade
+												</button>
+											</div>
+										</td>
+										<td className="px-4 py-2 text-right">
+											<div className="text-lg leading-none">
+												<span className="text-sm">Reward: </span>500
+											</div>
+										</td>
+									</tr>
+									<tr className="border-b border-gray-600">
+										<td className="px-4 py-2">
+											<div className="flex flex-col">
+												<span className="text-lg leading-none">Trade &gt; $2500 DOG...</span>
+												<button
+													className="mt-2 px-2 py-1 text-sm bg-[#393939] text-[#FE8A00] rounded border border-[#FE8A00] hover:bg-gray-600">
+													Trade
+												</button>
+											</div>
+										</td>
+										<td className="px-4 py-2 text-right">
+											<div className="text-lg leading-none">
+												<span className="text-sm">Reward: </span>500
+											</div>
+										</td>
+									</tr>
+									</tbody>
+								</table>
+							</div>
+							<div className="mt-6 mb-8 text-center">
+								<a href="#" className="text-xs text-[#FE8A00] underline">
+									Loyalty Program Terms and Conditions
+								</a>
+							</div>
+						</div>
+					</div>
+				);
+			}
 		}
-
-		if (error) {
-			return <div className="text-red-500">{error}</div>;
-		}
-
-		const totalCubePoints = 1160;
-
-		return (
-			<div className="flex flex-col h-full">
-				<div className="flex-grow overflow-hidden">
-					<div className="mb-4 text-center">
-						<div className="mt-10 text-sm text-gray-400">Total Cube Points</div>
-						<div className="mb-10 text-2xl font-bold text-white">{totalCubePoints.toLocaleString()}</div>
-					</div>
-					<div className="mb-8">
-						<table className="w-full bg-[#393939] text-white">
-							<thead className="sticky top-0 bg-[#393939]">
-							<tr>
-								<th className="px-4 py-2 text-left text-[#FE8A00]">Rewards</th>
-								<th className="px-4 py-2 text-right text-[#FE8A00] w-4/12">Points</th>
-							</tr>
-							</thead>
-							<tbody>
-							<tr className="border-b border-gray-600">
-								<td className="px-4 py-2">
-									<div className="flex flex-col">
-										<span className="text-lg leading-none">Invite Friends</span>
-										<button className="mt-2 px-2 py-1 text-sm bg-[#FE8A00] text-black rounded hover:bg-orange-600">
-											Send Referral
-										</button>
-									</div>
-								</td>
-								<td className="px-4 py-2 text-right">
-									<div className="text-lg leading-none">+80</div>
-								</td>
-							</tr>
-							<tr className="border-b border-gray-600">
-								<td className="px-4 py-2">
-									<div className="flex flex-col">
-										<span className="text-lg leading-none">Daily Check-in</span>
-										<span className="text-xs text-gray-400 mt-1">Streak: 5 Days</span>
-										<button
-											className={`mt-2 px-2 py-1 text-sm rounded ${claimStatus ? 'bg-white text-[#FE8A00] border border-[#FE8A00]' : 'bg-[#FE8A00] text-black hover:bg-orange-600'}`}
-											onClick={this.handleClaimReward}
-											disabled={claimStatus}
-										>
-											{claimStatus ? 'Claimed' : 'Claim Reward'}
-										</button>
-									</div>
-								</td>
-								<td className="px-4 py-2 text-right">
-									<div className="text-lg leading-none">+80</div>
-								</td>
-							</tr>
-							</tbody>
-						</table>
-					</div>
-					<div>
-						<table className="w-full bg-[#393939] text-white">
-							<thead className="sticky top-0 bg-[#393939]">
-							<tr>
-								<th className="px-4 py-2 text-left text-[#FE8A00]">Tasks</th>
-								<th className="px-4 py-2 text-right text-[#FE8A00] w-4/12">Points</th>
-							</tr>
-							</thead>
-							<tbody>
-							<tr className="border-b border-gray-600">
-								<td className="px-4 py-2">
-									<div className="flex flex-col">
-										<span className="text-lg leading-none">Completed Tasks</span>
-									</div>
-								</td>
-								<td className="px-4 py-2 text-right">
-									<div className="text-lg leading-none">+1000</div>
-								</td>
-							</tr>
-							<tr className="border-b border-gray-600">
-								<td className="px-4 py-2">
-									<div className="flex flex-col">
-										<span className="text-lg leading-none">Complete a trade &gt; $250</span>
-										<button className="mt-2 px-2 py-1 text-sm bg-[#393939] text-[#FE8A00] rounded border border-[#FE8A00] hover:bg-gray-600">
-											Trade
-										</button>
-									</div>
-								</td>
-								<td className="px-4 py-2 text-right">
-									<div className="text-lg leading-none">
-										<span className="text-sm">Reward: </span>500
-									</div>
-								</td>
-							</tr>
-							<tr className="border-b border-gray-600">
-								<td className="px-4 py-2">
-									<div className="flex flex-col">
-										<span className="text-lg leading-none">Trade &gt; $2500 ZEX</span>
-										<button className="mt-2 px-2 py-1 text-sm bg-[#393939] text-[#FE8A00] rounded border border-[#FE8A00] hover:bg-gray-600">
-											Trade
-										</button>
-									</div>
-								</td>
-								<td className="px-4 py-2 text-right">
-									<div className="text-lg leading-none">
-										<span className="text-sm">Reward: </span>500
-									</div>
-								</td>
-							</tr>
-							<tr className="border-b border-gray-600">
-								<td className="px-4 py-2">
-									<div className="flex flex-col">
-										<span className="text-lg leading-none">Trade &gt; $2500 DOG...</span>
-										<button className="mt-2 px-2 py-1 text-sm bg-[#393939] text-[#FE8A00] rounded border border-[#FE8A00] hover:bg-gray-600">
-											Trade
-										</button>
-									</div>
-								</td>
-								<td className="px-4 py-2 text-right">
-									<div className="text-lg leading-none">
-										<span className="text-sm">Reward: </span>500
-									</div>
-								</td>
-							</tr>
-							</tbody>
-						</table>
-					</div>
-					<div className="mt-6 mb-8 text-center">
-						<a href="#" className="text-xs text-[#FE8A00] underline">
-							Loyalty Program Terms and Conditions
-						</a>
-					</div>
-				</div>
-			</div>
-		);
-	}
-}
 
 const RewardsBehavior = (props: any) => {
 	const location = useLocation();
