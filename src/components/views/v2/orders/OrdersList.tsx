18317067c48c28ad94a4fa31b94b91b6432a2e66
Luan Carlos Gonçalves Silva <luan.silva@funttastic.com>
2024-08-27 23:53:02 -0300

    feat: adds no orders default state

--- src/components/views/v2/orders/OrdersList.tsx
+++ src/components/views/v2/orders/OrdersList.tsx
@@ -27,24 +27,30 @@ function OrdersList(props: OrderListProps) {
 				<div className="font-normal text-[#A2ACB0]">Price(USDC)</div>
 			</div>
 			<div>
-				{props.orders.map(order => (
-					<OrderCard key={order.id} order={order} canceledOrdersRef={props.canceledOrdersRef} fetchData={props.fetchData}/>
-				))}
-			</div>
-			<div className="py-4 flex justify-center items-center">
-				<Button value={"Cancel all"} type={ButtonType.Full} onClick={openModal} />
-
-				{isModalOpen && (
-					<div className="fixed inset-0 flex items-end justify-center bg-black bg-opacity-50">
-						<div className="bg-black w-full h-1/4 p-6 rounded-t-lg border-t border-white">
-							<p className="font-bold text-2xl text-center w-full">Cancel all orders?</p>
-							<div className="mt-4 flex flex-row justify-between w-full">
-								<Button value={"Yes, cancel"} type={ButtonType.Bordered} onClick={confirmCancelOrder} />
-								<Button value={"No, back"} type={ButtonType.Full} onClick={closeModal} />
-							</div>
+				{(!props.orders || props.orders.length === 0) ?  <div className="flex justify-center items-center h-40">You have no orders...</div> :
+					<div>
+						<div>
+							{props.orders.map(order => (
+								<OrderCard key={order.id} order={order} canceledOrdersRef={props.canceledOrdersRef}
+													 fetchData={props.fetchData}/>
+							))}
 						</div>
-					</div>
-				)}
+						<div className="py-4 flex justify-center items-center">
+							<Button value={"Cancel all"} type={ButtonType.Full} onClick={openModal}/>
+
+							{isModalOpen && (
+								<div className="fixed inset-0 flex items-end justify-center bg-black bg-opacity-50">
+									<div className="bg-black w-full h-1/4 p-6 rounded-t-lg border-t border-white">
+										<p className="font-bold text-2xl text-center w-full">Cancel all orders?</p>
+										<div className="mt-4 flex flex-row justify-between w-full">
+											<Button value={"Yes, cancel"} type={ButtonType.Bordered} onClick={confirmCancelOrder}/>
+											<Button value={"No, back"} type={ButtonType.Full} onClick={closeModal}/>
+										</div>
+									</div>
+								</div>
+							)}
+						</div>
+					</div>}
 			</div>
 		</div>
 
