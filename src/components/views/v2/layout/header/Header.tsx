b11ad7e15626be2f64094a7bfa42aecece9c7ba3
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-09-04 16:46:10 -0300

    Removed header component.

--- src/components/views/v2/layout/header/Header.tsx
+++ src/components/views/v2/layout/header/Header.tsx
@@ -1,12 +1,13 @@
 // import logo from 'src/assets/images/logo/exchange.png';
 import React from 'react';
 // import { useNavigate } from 'react-router-dom';
-import { AppBar, IconButton, styled, Toolbar, Typography } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { AppBar,  styled } from '@mui/material';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+// import ClearIcon from '@mui/icons-material/Clear';
 // import { Constant } from 'model/enum/constant.ts';
 import { connect } from 'react-redux';
 import { dispatch } from 'model/state/redux/store';
-import { getCurrentRouteTitle } from 'components/views/v2/utils/utils';
+// import { getCurrentRouteTitle } from 'components/views/v2/utils/utils';
 
 // @ts-ignore
 // noinspection JSUnusedLocalSymbols
@@ -25,11 +26,11 @@ const mapStateToProps = (state: any, props: any) => ({
 export const HeaderStructure = (props: any) => {
 	// const navigate = useNavigate();
 
+	// @ts-ignore
 	const toggleMenu = () => (event: React.KeyboardEvent | React.MouseEvent) => {
 		if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
 			return;
 		}
-
 		dispatch('app.toggleMenu', !props.isMenuOpen);
 	};
 
@@ -41,39 +42,55 @@ export const HeaderStructure = (props: any) => {
 
 	return (
 		<Style position="static">
-			<Toolbar
-				className="flex items-center justify-between p-4"
-				sx={{ justifyContent: 'space-between' }}
-			>
-				{/*<div className="flex items-center" onClick={handleLogoClick}>*/}
-				{/*	<img src={logo} alt="Logo" className="h-10 w-10 mr-2"/>*/}
-				{/*</div>*/}
-				{/*<Typography*/}
-				{/*	variant="h6"*/}
-				{/*	component="div"*/}
-				{/*	sx={{*/}
-				{/*		flexGrow: 1,*/}
-				{/*		textAlign: 'center',*/}
-				{/*		position: 'absolute',*/}
-				{/*		left: '50%',*/}
-				{/*		transform: 'translateX(-50%)'*/}
-				{/*	}}*/}
-				{/*>*/}
-				{/*	{getCurrentRouteTitle()}*/}
-				{/*</Typography>*/}
-				<IconButton
-					size="large"
-					edge="end"
-					color="inherit"
-					aria-label="menu"
-					sx={{ ml: 'auto' }}
-					onClick={toggleMenu()}
-				>
-					<MoreVertIcon />
-				</IconButton>
-			</Toolbar>
-		</Style>
-	);
+		{/*	<Toolbar*/}
+		{/*		className="flex items-center justify-between p-4"*/}
+		{/*		sx={{justifyContent: 'space-between'}}*/}
+		{/*	>*/}
+		{/*		/!*<div style={{position: 'relative', flexGrow: 1}}>*!/*/}
+		{/*		/!*	<Typography*!/*/}
+		{/*		/!*		component="div"*!/*/}
+		{/*		/!*		sx={{*!/*/}
+		{/*		/!*			fontSize: '0.8rem',*!/*/}
+		{/*		/!*			textAlign: 'left',*!/*/}
+		{/*		/!*			position: 'relative',*!/*/}
+		{/*		/!*			left: '55%',*!/*/}
+		{/*		/!*			transform: 'translateX(-50%)'*!/*/}
+		{/*		/!*		}}*!/*/}
+		{/*		/!*	>*!/*/}
+		{/*		/!*		<ClearIcon*!/*/}
+		{/*		/!*			sx={{*!/*/}
+		{/*		/!*				fontSize: '1.1rem',*!/*/}
+		{/*		/!*				position: 'absolute',*!/*/}
+		{/*		/!*				left: '-4%',*!/*/}
+		{/*		/!*				transform: 'translateX(-50%)'*!/*/}
+		{/*		/!*			}}*!/*/}
+		{/*		/!*		/>*!/*/}
+		{/*		/!*		{getCurrentRouteTitle()}*!/*/}
+		{/*		/!*	</Typography>*!/*/}
+		{/*		/!*</div>*!/*/}
+		{/*		<IconButton*/}
+		{/*			size="large"*/}
+		{/*			edge="end"*/}
+		{/*			color="inherit"*/}
+		{/*			aria-label="menu"*/}
+		{/*			sx={{ml: 'auto'}}*/}
+		{/*			onClick={toggleMenu()}*/}
+		{/*		>*/}
+		{/*			<MoreVertIcon/>*/}
+		{/*		</IconButton>*/}
+		{/*		<div*/}
+		{/*			style={{*/}
+		{/*				borderBottom: '1px  solid #202020',*/}
+		{/*				width: '100%',*/}
+		{/*				left: 0,*/}
+		{/*				position: 'absolute',*/}
+		{/*				bottom: 20,*/}
+		{/*			}}*/}
+		{/*		/>*/}
+		{/*</Toolbar>*/}
+</Style>
+)
+	;
 }
 
 export const Header = connect(mapStateToProps)(HeaderStructure);
