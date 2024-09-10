6a0a3c911a61a0a424fa8a3187988211829ef31d
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-09-10 20:17:03 -0300

    adjusting layout

--- src/components/views/v2/rewards/Rewards.tsx
+++ src/components/views/v2/rewards/Rewards.tsx
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux';
 import { toast } from 'react-toastify';
 import axios from 'axios';
-import { Box, Button, styled } from '@mui/material';
+import { Box, Button, styled, LinearProgress } from '@mui/material';
+import { TrendingUp, BookmarkAddedOutlined, EmojiEventsOutlined, SignalCellularAltOutlined }from '@mui/icons-material';
 import { executeAndSetInterval } from 'model/service/recurrent';
 import { dispatch } from 'model/state/redux/store';
 import { Base, BaseProps, BaseState, withHooks } from 'components/base/Base';
@@ -64,29 +65,34 @@ class Structure extends Base<Props, State> {
 					<Title>Rewards</Title>
 					<Row>
 						<LargeRowTitle>Points</LargeRowTitle>
-						<LargeRowValue>5,213,564</LargeRowValue>
+						<RowValue>5,213,564</RowValue>
 					</Row>
 				</StyledBox>
 
 				<StyledBox>
-					<Title>Progress</Title>
-					<CompactRow>
-						<RowTitle>Deposits</RowTitle>
-						<RowValue>$8,500</RowValue>
-					</CompactRow>
-					<CompactRow>
-						<RowTitle>Wtd Vol</RowTitle>
-						<RowValue>$486,321</RowValue>
-					</CompactRow>
-					<CompactRow>
-						<RowTitle>Volume</RowTitle>
-						<RowValue>$2,435,934</RowValue>
-					</CompactRow>
+						<TitleOne>Progress <SignalCellularAltOutlined fontSize="medium" style={{ marginLeft: '14.5em' }}/> </TitleOne>
+						<CompactRowOne>
+							<RowTitle>Deposits</RowTitle>
+							<RowValue>$8,500</RowValue>
+						</CompactRowOne>
+						<CompactRowOne>
+							<RowTitle>Wtd Vol</RowTitle>
+							<RowValue>$486,321</RowValue>
+						</CompactRowOne>
+						<CompactRowOne>
+							<RowTitle>Volume</RowTitle>
+							<RowValue>$2,435,934</RowValue>
+						</CompactRowOne>
+					<LinearProgress variant="indeterminate" style={{marginBottom:'1.25em'}} />
 				</StyledBox>
+
 
 				<RowContainer>
 					<StyledFullBox>
-						<Title>Tier</Title>
+						<Title>
+							Tier
+							<EmojiEventsOutlined fontSize="xsmall" style={{ marginLeft: '7.25em' }}/>
+						</Title>
 						<CompactRow>
 							<LargeRowValue>Gold</LargeRowValue>
 						</CompactRow>
@@ -96,9 +102,16 @@ class Structure extends Base<Props, State> {
 					</StyledFullBox>
 
 					<StyledFullBox>
-						<Title>Loyalty Score</Title>
+						<Title>
+							Loyalty Score
+							<BookmarkAddedOutlined fontSize="xsmall" style={{ marginLeft: '3.25em' }}/>
+						</Title>
 						<Row>
 							<LargeRowValue>236</LargeRowValue>
+						</Row>
+						<Row>
+
+							<RowSubValue><TrendingUp fontSize="small" /> +25 / 24 h</RowSubValue>
 						</Row>
 					</StyledFullBox>
 				</RowContainer>
@@ -184,7 +197,7 @@ class Structure extends Base<Props, State> {
 					),*/
 				]);
 
-				responses.forEach((response) => {
+				responses.forEach((response: any) => {
 					if (response.status !== 200) {
 						if (response.data?.title) {
 							const message = response.data.title;
@@ -240,10 +253,10 @@ const StyledContainer = styled(Box)`
 `;
 
 const StyledBox = styled(Box)`
-	background-color: #333;
-	padding: 20px;
-	border-radius: 8px;
+	padding: 0.7em;
 	position: relative;
+	border-radius: 23px;
+	background-color: rgba(31, 32, 30, 0.64);
 `;
 
 const StyledFullBox = styled(StyledBox)`
@@ -252,62 +265,91 @@ const StyledFullBox = styled(StyledBox)`
 
 const Title = styled('div')`
 	font-size: 12px;
-	font-weight: bold;
+	font-weight: normal;
 	position: absolute;
-	top: 8px;
-	left: 10px;
+	top: 12px;
+	left: 16px;
+	padding: 0 0.25em ;
 `;
 
 const Row = styled('div')`
 	display: flex;
 	justify-content: space-between;
-	padding: 10px 0;
-	margin-top: 20px;
+	padding: 0.25em 0;
+	margin-top: 1em;
 `;
 
 const CompactRow = styled(Row)`
-	padding: 5px 0;
+	padding: 8px 0;
 	margin-top: 10px;
 `;
 
+const TitleOne = styled('div')`
+	top: 0.1em;
+	left: 0.5em;
+	position: relative;
+	font-size: 12px;
+	font-weight: normal;
+	padding-bottom: 6em;
+`;
+
+const CompactRowOne = styled(Row)`
+	color: rgba(170, 170, 170, 0.55);
+	margin: 0;
+	position: relative;
+	display: flex;
+	padding: 0;
+	top: -1.5em;
+	left: 0.4em;
+`;
+
 const RowTitle = styled('div')`
-	font-size: 14px;
+	display: flex;
+	font-size: 11px;
 `;
 
 const RowValue = styled('div')`
-	font-size: 14px;
-	font-weight: bold;
+	font-size: 12px;
+	font-weight:normal;
 `;
 
 const LargeRowTitle = styled(RowTitle)`
 	font-size: 20px;
-	font-weight: bold;
+	font-weight: lighter;
 `;
 
 const LargeRowValue = styled(RowValue)`
-	font-size: 20px;
-	font-weight: bold;
+	font-size: 1.3em;
+	margin-top: 4em;
+	font-weight: lighter;
 `;
 
 const RowSubValue = styled('div')`
-	font-size: 12px;
-	color: #aaa;
+	color: rgba(170, 170, 170, 0.55);
+	font-size: 11px;
+	margin-top: -1em;
 `;
 
 const RowContainer = styled(Box)`
-	display: flex;
 	gap: 20px;
+	height: 12em;
+	display: flex;
 `;
 
 const ButtonsRow = styled(Box)`
+	gap: 7.5em;
 	display: flex;
-	gap: 10px;
-	margin-top: 10px;
+
+	margin-top: 7.5em;
 `;
 
 const StyledButton = styled(Button)`
-	background-color: #2b2b2b;
 	color: white;
+	padding: 0.7em 1.5em;
+	font-size: 8px;
+	font-style: normal;
+	border-radius: 5em;
+	background-color: #2b2b2b;
 
 	&:hover {
 		background-color: #1f1f1f;
@@ -315,6 +357,10 @@ const StyledButton = styled(Button)`
 `;
 
 const InvertedButton = styled(Button)`
+	padding: 0.5em 2em;
+	font-size: 8px;
+	text-align: left;
+	border-radius: 5em;
 	background-color: white;
 	color: black;
 
