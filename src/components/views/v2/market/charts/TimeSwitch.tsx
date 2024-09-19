e4bdaced3ef0ddf413c5ca01176c0421e7c45361
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-09-19 20:09:45 -0300

    Fixing the TimeSwitch.tsx component.

--- src/components/views/v2/market/charts/TimeSwitch.tsx
+++ src/components/views/v2/market/charts/TimeSwitch.tsx
@@ -1,23 +1,37 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Grid } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-const StyledButton = styled(Button)<{ isSelected: boolean }>(({ isSelected }) => ({
-	backgroundColor: isSelected ? '#ffffff' : '#181818',
-	color: isSelected ? '#000000' : '#ffffff',
-	borderRadius: '50%',
-	minWidth: '48px',
-	height: '48px',
-	fontWeight: 'bold',
-	textTransform: 'none', // This ensures the text case is not changed
-	'&:hover': {
-		backgroundColor: isSelected ? '#ffffff' : '#303030',
-	},
-}));
+interface GranularityButtonProps {
+	isSelected: boolean;
+	children: React.ReactNode;
+	onClick: () => void;
+}
+
+const GranularityButtonStructure = ({ isSelected, children, ...otherProps }: GranularityButtonProps) => {
+	return <Button {...otherProps}>{children}</Button>;
+};
+
+// noinspection JSUnusedLocalSymbols
+const GranularityButton = styled(GranularityButtonStructure)<{ isSelected: boolean }>(
+	// @ts-ignore
+	({ isSelected, theme }) => ({
+		backgroundColor: isSelected ? '#ffffff' : '#181818',
+		color: isSelected ? '#000000' : '#ffffff',
+		borderRadius: '50%',
+		minWidth: '3rem',
+		height: '3rem',
+		fontWeight: 'bold',
+		textTransform: 'none',
+		'&:hover': {
+			backgroundColor: isSelected ? '#ffffff' : '#303030',
+		},
+	})
+);
 
 interface TimeSwitchProps {
 	defaultGranularity: '1s' | '1m' | '1h' | '4h' | '1d';
-	onGranularityChange: (time: string) => void; // Callback function prop
+	onGranularityChange: (time: string) => void;
 }
 
 export const TimeSwitch = ({ defaultGranularity = '1h', onGranularityChange }: TimeSwitchProps) => {
@@ -34,12 +48,12 @@ export const TimeSwitch = ({ defaultGranularity = '1h', onGranularityChange }: T
 		<Grid container spacing={2} justifyContent="center">
 			{timeOptions.map((time) => (
 				<Grid item key={time}>
-					<StyledButton
+					<GranularityButton
 						isSelected={selectedGranularity === time}
 						onClick={() => handleGranularityChange(time)}
 					>
 						{time}
-					</StyledButton>
+					</GranularityButton>
 				</Grid>
 			))}
 		</Grid>
